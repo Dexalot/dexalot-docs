d435ef954a39d7ff8ad6358359a37232edc87c1e
BradMcFall <44811837+BradMcFall@users.noreply.github.com>
2022-10-09 20:23:42 -0400

    content tree

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -113,7 +113,7 @@ export default defineUserConfig({
         icon: "overflow",
         collapsable: true,
         link: "contents",
-        prefix: "contents/",
+        prefix: "contents/articles",
         children: [
           {
             text: "Litepaper",
